f6cb7aae8e973836cef2fa9194515ce8207f7e29
Matheus <matheus.evers@ab-inbev.com>
2020-05-02 19:31:02 -0300

    feed done

--- src/pages/FeedPage/FeedPage.js
+++ src/pages/FeedPage/FeedPage.js
@@ -19,9 +19,27 @@ import SettingsIcon from "@material-ui/icons/Settings";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 import MenuIcon from "@material-ui/icons/Menu";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
+import ArticleCard from "../../components/ArticleCard/ArticleCard";
+import { Link } from "react-router-dom";
 import "./FeedPage.scss";
-const drawerWidth = 240;
 
+const drawerWidth = 240;
+const title = [
+  "Ações do governo custarão ao menos R$ 350 bilhões em 2020",
+  "Wall Street fecha em queda",
+  "Petróleo registra a primeira semana de alta",
+  "Governo prepara socorro para empresas que prestam serviços de apoio em aeroportos",
+  "Preços do petróleo sobem nesta quinta-feira",
+  "Bolsas dos EUA fecham abril com maior ganho em décadas",
+];
+const description = [
+  "Tesouro diz que R$ 60 bilhões já foram gastos com medidas de enfrentamento à crise gerada pelo novo coronavírus. Estimativa é de que dívida pública suba para 90% do PIB.",
+  "Wall Street sofreu queda acentuada nesta sexta-feira após o presidente Donald Trump reviver uma ameaça de novas tarifas contra a China em resposta à pandemia do novo coronavírus que freou as economias globais.",
+  "Os contratos futuros do petróleo fecharam sem direção única nesta sexta-feira (1º), mas anotaram a primeira semana de ganhos após três perdas semanais consecutivas.",
+  "Setor que empregava cerca de 40 mil pessoas antes da crise e presta serviços como manuseio de bagagens e cargas aéreas já registra demissões e atraso em salários.",
+  "Os preços do petróleo tiveram novo salto nesta quinta-feira (30), após diversos produtores afirmarem que vão reduzir bombeamento e sinais indicarem que o excesso de oferta nos Estados Unidos não está avançando tão rápido quanto muitos temiam, fatores que levaram um tom otimista ao mercado no final de um dos meses mais voláteis da história da commodity.",
+  "Alta foi impulsionada pelas expectativas de que a economia deve começar a se recuperar em breve das severas restrições estabelecidas para conter a pandemia de coronavírus.",
+];
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
@@ -74,23 +92,34 @@ function FeedPage(props) {
     <div>
       <div className={classes.toolbar} />
       <List>
-        <ListItem button key="logo-item" style={{ justifyContent: "center" }}>
-          <img
-            className="logo-item"
-            src={`${process.env.PUBLIC_URL}/assets/images/logo_white.png`}
-          />
-        </ListItem>
-        {["Configuração", "Minha Conta"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon style={{ color: "white" }}>
-              {index % 2 === 0 ? <SettingsIcon /> : <AccountCircleIcon />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
+        <Link to="/">
+          <ListItem button key="logo-item" style={{ justifyContent: "center" }}>
+            <img
+              className="logo-item"
+              src={`${process.env.PUBLIC_URL}/assets/images/logo_white.png`}
+            />
           </ListItem>
-        ))}
+        </Link>
+        <ListItem button disabled key="config-item">
+          <ListItemIcon style={{ color: "white" }}>
+            <SettingsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Configuração" />
+        </ListItem>
+        <Link to="/planilha">
+          <ListItem button key="minha-conta-item">
+            <ListItemIcon style={{ color: "white" }}>
+              <AccountCircleIcon />
+            </ListItemIcon>
+            <ListItemText
+              style={{ color: "white", textDecoration: "none" }}
+              primary="Minha Conta"
+            />
+          </ListItem>
+        </Link>
       </List>
       <Divider />
-      <ListItem button key="categorias-item">
+      <ListItem key="categorias-item">
         <ListItemText
           disableTypography
           primary={
@@ -106,9 +135,11 @@ function FeedPage(props) {
           }
         />
       </ListItem>
+      <ListItem button>
+        <ListItemText primary="Ações" />
+      </ListItem>
       <List>
         {[
-          "Ações",
           "Mercado financeiro",
           "Renda fixa",
           "Renda variavel",
@@ -117,7 +148,7 @@ function FeedPage(props) {
           "Categoria 3",
           "Categoria 4",
         ].map((text, index) => (
-          <ListItem button key={text}>
+          <ListItem button disabled key={text}>
             <ListItemText primary={text} />
           </ListItem>
         ))}
@@ -143,7 +174,7 @@ function FeedPage(props) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-            Titulo da Categoria
+            Ações
           </Typography>
         </Toolbar>
       </AppBar>
@@ -181,40 +212,19 @@ function FeedPage(props) {
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <Container>
-          <Typography paragraph>
-            <img
-              className="logo-item"
-              src={`${process.env.PUBLIC_URL}/assets/images/article.png`}
-            />
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
+          {title.map((title, index) => {
+            return (
+              <ArticleCard
+                imgSrc={
+                  index % 2 === 0
+                    ? `${process.env.PUBLIC_URL}/assets/images/article0.png`
+                    : `${process.env.PUBLIC_URL}/assets/images/article2.png`
+                }
+                articleTitle={title}
+                articleText={description[index]}
+              />
+            );
+          })}
         </Container>
       </main>
     </div>
